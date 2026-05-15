86a442197f85a7379f81abbe20a63043a999b782
kaurparmar <arminderdeepkaur1@gmail.com>
2026-05-15 23:08:17 +0530

    Fixed missing policyApplications relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Claim::class);
     }
+    public function policyApplications()
+{
+    return $this->hasMany(Policy::class, 'user_id');
+}
 }
